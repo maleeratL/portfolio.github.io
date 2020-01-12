8d6d39a8c4a01b52d3c343bc3b68c9bfe4c2b0d6
maleeratL <maleerat.l.123@gmail.com>
2020-01-12 23:13:21 +1300

    fix default link to top of the page from close button

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.display = "none";
 }
+
+// var mybutton = document.getElementById("myBtn");
+//
+// window.onscroll = function() {scrollFunction()};
+//
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+//
+// function GoTopFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
